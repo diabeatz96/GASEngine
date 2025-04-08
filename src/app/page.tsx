9cfbdb0890b624ed5327b16251254835cd1c97d5
AladdinOmar61 <55258266+AladdinOmar61@users.noreply.github.com>
2025-04-07 23:14:07 -0400

    created home page to match design
    will work on responsiveness next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
-// import Form from "next/form";
+import { Search } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="flex-1 justify-center items-center h-[100vh]">
-      <h1 className="text-center text-black text-7xl mt-5">GAS</h1>
-      <div className="flex justify-center">
-        <input className="bg-[#B98DE7] w-[50%] pl-3 h-11 rounded-full mt-10 text-black text-lg focus:outline-0" type="text"></input>
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex-col justify-center items-center">
+        <h1
+          className="text-center text-black/70 text-9xl mb-12"
+          style={{ fontFamily: "var(--font-jersey)" }}
+        >
+          GAS Engine
+        </h1>
+        <div>
+          <p className="text-center">find your next game!</p>
+          <div className="relative">
+            <input
+              className="bg-[#B98DE7] shadow-lg w-[45vw] pl-3 h-11 my-5 rounded-full text-white font-bold text-lg focus:outline-0"
+              style={{ boxShadow: "3px 6px 0px rgb(110, 56, 165)" }}
+              type="text"
+            ></input>
+            <Search
+              className="absolute top-1/2 right-4 transform -translate-y-1/2"
+              strokeWidth={3}
+              color="white"
+            />
+          </div>
+          <p className="text-center">search by filter</p>
+        </div>
       </div>
     </div>
   );
